9b1611000f0a94f8fc2c5c369ab4d4420964ec33
Stason4eg <33861293+Stason4eg@users.noreply.github.com>
2018-06-26 16:09:40 +0300

    Update WebDashboardForm.aspx.vb

--- VB/WebDashboardForm.aspx.vb
+++ VB/WebDashboardForm.aspx.vb
@@ -16,49 +16,47 @@ Namespace WebDashboard_CustomizeExport
         End Sub
 
         Protected Sub ASPxWebDashboard1_CustomizeExportDocument(ByVal sender As Object, ByVal e As CustomizeExportDocumentWebEventArgs)
-            If True Then
-                If e.ExportAction = DashboardExportAction.ExportToExcel Then
-                    Dim ms As New MemoryStream()
-                    e.Stream.Position = 0
+            If e.ExportAction = DashboardExportAction.ExportToExcel Then
+                Dim ms As New MemoryStream()
+                e.Stream.Position = 0
 
-                    Dim workbook As New Workbook()
-                    If e.ExcelExportOptions.Format = ExcelFormat.Xlsx Then
-                        workbook.LoadDocument(e.Stream, DocumentFormat.Xlsx)
-                    ElseIf e.ExcelExportOptions.Format = ExcelFormat.Xls Then
-                        workbook.LoadDocument(e.Stream, DocumentFormat.Xls)
-                    ElseIf e.ExcelExportOptions.Format = ExcelFormat.Csv Then
-                        workbook.LoadDocument(e.Stream, DocumentFormat.Csv)
-                    End If
-                    For Each sheet As Worksheet In workbook.Worksheets
-                        ' Export to CSV without images, cell merging and coloring. 
-                        If e.ExcelExportOptions.Format = ExcelFormat.Csv Then
-                            sheet.Rows.Insert(0)
-                            Dim textCell As Cell = sheet.Cells(0, 0)
-                            textCell.Value = "Custom Document Header"
-                        ' Export to the Excel workbook with images, cell merging and coloring.
-                        Else
-                            sheet.Rows.Insert(0, 3)
-                            sheet.Pictures.AddPicture(My.Resources.dxLogo, sheet.Range.FromLTRB(0, 0, 5, 2), True)
-                            Dim textCell As Cell = sheet.Cells(0, 5)
-                            textCell.Value = "Custom Document Header"
-
-                            sheet.MergeCells(sheet.Range.FromLTRB(5, 0, 8, 0))
-                            Dim formatting As Formatting = textCell.BeginUpdateFormatting()
-                            formatting.Fill.BackgroundColor = Color.LightBlue
-                            textCell.EndUpdateFormatting(formatting)
-                            textCell.Alignment.Horizontal = SpreadsheetHorizontalAlignment.Center
-                        End If
-
-                    Next sheet
-                    If e.ExcelExportOptions.Format = ExcelFormat.Xlsx Then
-                        workbook.SaveDocument(ms, DocumentFormat.Xlsx)
-                    ElseIf e.ExcelExportOptions.Format = ExcelFormat.Xls Then
-                        workbook.SaveDocument(ms, DocumentFormat.Xls)
-                    ElseIf e.ExcelExportOptions.Format = ExcelFormat.Csv Then
-                        workbook.SaveDocument(ms, DocumentFormat.Csv)
-                    End If
-                    e.Stream = ms
+                Dim workbook As New Workbook()
+                If e.ExcelExportOptions.Format = ExcelFormat.Xlsx Then
+                    workbook.LoadDocument(e.Stream, DocumentFormat.Xlsx)
+                ElseIf e.ExcelExportOptions.Format = ExcelFormat.Xls Then
+                    workbook.LoadDocument(e.Stream, DocumentFormat.Xls)
+                ElseIf e.ExcelExportOptions.Format = ExcelFormat.Csv Then
+                    workbook.LoadDocument(e.Stream, DocumentFormat.Csv)
                 End If
+                For Each sheet As Worksheet In workbook.Worksheets
+                    ' Export to CSV without images, cell merging and coloring. 
+                    If e.ExcelExportOptions.Format = ExcelFormat.Csv Then
+                        sheet.Rows.Insert(0)
+                        Dim textCell As Cell = sheet.Cells(0, 0)
+                        textCell.Value = "Custom Document Header"
+                    ' Export to the Excel workbook with images, cell merging and coloring.
+                    Else
+                        sheet.Rows.Insert(0, 3)
+                        sheet.Pictures.AddPicture(My.Resources.dxLogo, sheet.Range.FromLTRB(0, 0, 5, 2), True)
+                        Dim textCell As Cell = sheet.Cells(0, 5)
+                        textCell.Value = "Custom Document Header"
+
+                        sheet.MergeCells(sheet.Range.FromLTRB(5, 0, 8, 0))
+                        Dim formatting As Formatting = textCell.BeginUpdateFormatting()
+                        formatting.Fill.BackgroundColor = Color.LightBlue
+                        textCell.EndUpdateFormatting(formatting)
+                        textCell.Alignment.Horizontal = SpreadsheetHorizontalAlignment.Center
+                    End If
+
+                Next sheet
+                If e.ExcelExportOptions.Format = ExcelFormat.Xlsx Then
+                    workbook.SaveDocument(ms, DocumentFormat.Xlsx)
+                ElseIf e.ExcelExportOptions.Format = ExcelFormat.Xls Then
+                    workbook.SaveDocument(ms, DocumentFormat.Xls)
+                ElseIf e.ExcelExportOptions.Format = ExcelFormat.Csv Then
+                    workbook.SaveDocument(ms, DocumentFormat.Csv)
+                End If
+                e.Stream = ms
             End If
         End Sub
 
